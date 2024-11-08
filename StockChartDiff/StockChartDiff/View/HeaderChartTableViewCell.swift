8d339860155ec18fefa2d1d24d42419216420544
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2024-11-08 22:23:42 +0700

    Setup chart

--- StockChartDiff/StockChartDiff/View/HeaderChartTableViewCell.swift
+++ StockChartDiff/StockChartDiff/View/HeaderChartTableViewCell.swift
@@ -13,6 +13,7 @@ class HeaderChartTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.setupLineChart()
+        self.applyThemes()
     }
 
     static var nib: UINib {
@@ -33,6 +34,9 @@ class HeaderChartTableViewCell: UITableViewCell {
     public func setupChartData(data: StockChartsList) {
         var dataSets: [LineChartDataSet] = []
 
+        self.priceMin = data.map({ $0.open }).min() ?? 0.00
+        self.priceMax = data.map({ $0.open }).max() ?? 0.00
+
         let todayOpenPrice = data.filter { data in
             data.date.contains(APIClient.getCurrentDateToString())
         }.first?.open ?? 0.00
@@ -51,40 +55,51 @@ class HeaderChartTableViewCell: UITableViewCell {
                 } else if item.open < todayOpenPrice {
                     dataSet.colors = [.red]
                 } else {
-                    dataSet.colors = [.yellow]
+                    dataSet.colors = [.green]
                 }
             } else {
-                dataSet.colors = [.black]
+                dataSet.colors = [.white]
             }
 
             dataSet.drawCirclesEnabled = false
             dataSet.mode = .cubicBezier
-            dataSet.lineWidth = 1
-
+            dataSet.lineWidth = 2
             dataSets.append(dataSet)
         }
 
-        
+        let yAxis = self.chartView.leftAxis
+        yAxis.axisMaximum = self.priceMax
+        yAxis.axisMinimum = self.priceMin
 
         let data = LineChartData(dataSets: dataSets)
-
         self.chartView.data = data
     }
 
+    private func applyThemes() {
+        self.contentView.backgroundColor = .black
+        self.backgroundColor = .black
+    }
+
     private func setupLineChart() {
-        self.chartView.backgroundColor = .white
         self.chartView.pinchZoomEnabled = false
         self.chartView.dragEnabled = false
         self.chartView.doubleTapToZoomEnabled = false
         self.chartView.legend.enabled = false
+        self.chartView.backgroundColor = .black
+        self.chartView.rightAxis.enabled = false
 
         let xAxis = self.chartView.xAxis
         xAxis.drawAxisLineEnabled = true
         xAxis.drawGridLinesEnabled = false
+        xAxis.drawLabelsEnabled = false
         xAxis.granularity = 1
+        xAxis.setLabelCount(0, force: false)
 
         let yAxis = self.chartView.leftAxis
         yAxis.drawGridLinesEnabled = false
         yAxis.drawGridLinesBehindDataEnabled = false
+        yAxis.drawAxisLineEnabled = false
+        yAxis.labelTextColor = .white
+        xAxis.setLabelCount(4, force: false)
     }
 }
