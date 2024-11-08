943b21982ba096a49f69fb06d51004c6b21c3a43
jay-suppanat <119741800+jay-suppanat@users.noreply.github.com>
2024-11-08 10:37:31 +0700

    Init StockChartDiff project

---
+++ StockChartDiff/StockChartDiff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockChartDiff
+//
+//  Created by Suppanat Chinthumrucks on 8/11/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StockChartDiff/StockChartDiffTests/StockChartDiffTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockChartDiffTests.swift
+//  StockChartDiffTests
+//
+//  Created by Suppanat Chinthumrucks on 8/11/2567 BE.
+//
+
+import Testing
+@testable import StockChartDiff
+
+struct StockChartDiffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
